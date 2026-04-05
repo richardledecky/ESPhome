f40afaa3bc98f6e083c6f6ebe37516a5084f2e65
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 20:43:42 +0200

    Update __init__.py

--- components/rc_switch_component/__init__.py
+++ components/rc_switch_component/__init__.py
@@ -1,17 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 
 CODEOWNERS = ["@richardledecky"]
 DEPENDENCIES = []
 AUTO_LOAD = []
 
-# Namespace
 ns = cg.esphome_ns.namespace("rc_switch_component")
-RCSwitchComponent = ns.class_("RCSwitchComponent", cg.Component)
+
+RCSwitchTransmitter = ns.class_("RCSwitchTransmitter", cg.Component)
+SendRCSwitchAction = ns.class_("SendRCSwitchAction", cg.Action)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(RCSwitchComponent),
+    cv.GenerateID(): cv.declare_id(RCSwitchTransmitter),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
