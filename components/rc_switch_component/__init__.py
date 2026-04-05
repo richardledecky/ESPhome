5299e3e02ac590782e930b7d6c9b283d49624ece
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 20:42:40 +0200

    Update __init__.py

--- components/rc_switch_component/__init__.py
+++ components/rc_switch_component/__init__.py
@@ -1,28 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_CODE
+from esphome import pins
 
-rc_switch_ns = cg.esphome_ns.namespace("rc_switch_component")
-SendRCSwitchAction = rc_switch_ns.class_("SendRCSwitchAction", cg.Action)
+CODEOWNERS = ["@richardledecky"]
+DEPENDENCIES = []
+AUTO_LOAD = []
 
-CONF_GPIO = "gpio"
+# Namespace
+ns = cg.esphome_ns.namespace("rc_switch_component")
+RCSwitchComponent = ns.class_("RCSwitchComponent", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(SendRCSwitchAction),
-        cv.Required(CONF_CODE): cv.uint32_t,
-        cv.Optional(CONF_GPIO, default=23): cv.int_,
-    }
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(RCSwitchComponent),
+}).extend(cv.COMPONENT_SCHEMA)
 
-
-@cg.register_action("rc_switch_component.send", SendRCSwitchAction, CONFIG_SCHEMA)
-async def rc_switch_send_to_code(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id, template_arg)
-    cg.add(var.set_code(config[CONF_CODE]))
-    cg.add(var.set_gpio(config[CONF_GPIO]))
-    # pridáme C++ súbory a knižnicu
-    cg.add_library("rc-switch", None)
-    cg.add_source_file("rc_switch_component.cpp")
-    cg.add_source_file("rc_switch_component.h")
-    return var
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
