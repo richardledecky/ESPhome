8f5544f3582eb385bbb33a6149ce4961b2e478f2
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 20:43:54 +0200

    Update rc_switch_component.h

--- components/rc_switch_component/rc_switch_component.h
+++ components/rc_switch_component/rc_switch_component.h
@@ -1,26 +1,41 @@
 #pragma once
 
-#include "esphome/core/automation.h"
 #include "esphome/core/component.h"
-#include "RCSwitch.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/log.h"
+#include <RCSwitch.h>
 
 namespace esphome {
 namespace rc_switch_component {
 
-class SendRCSwitchAction : public esphome::Action<> {
+class RCSwitchTransmitter : public Component {
  public:
-  void set_code(uint32_t code) { code_ = code; }
-  void set_gpio(int gpio) { gpio_ = gpio; }
+  RCSwitchTransmitter() = default;
 
-  template<typename... Ts> void play(Ts... x) {
-    RCSwitch sw;
-    sw.enableTransmit(gpio_);
-    sw.send(code_, 24);
+  void setup() override {
+    ESP_LOGI("rc_switch", "Initializing RC Switch transmitter");
+    transmitter_.enableTransmit(12);  // default TX pin (GPIO12)
+  }
+
+  void send_code(uint32_t code) {
+    ESP_LOGI("rc_switch", "Sending RC code: %u", code);
+    transmitter_.send(code, 24);
   }
 
  protected:
-  uint32_t code_{0};
-  int gpio_{23};
+  RCSwitch transmitter_;
+};
+
+class SendRCSwitchAction : public Action<void> {
+ public:
+  RCSwitchTransmitter *parent;
+  uint32_t code;
+
+  void play(Ts... x) override {
+    if (parent != nullptr) {
+      parent->send_code(code);
+    }
+  }
 };
 
 }  // namespace rc_switch_component
