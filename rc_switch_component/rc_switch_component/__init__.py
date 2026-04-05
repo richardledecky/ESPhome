e36a916d9799e691fc7fdf570882b2fb9b2618bc
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 18:54:54 +0200

    Update __init__.py

--- rc_switch_component/rc_switch_component/__init__.py
+++ rc_switch_component/rc_switch_component/__init__.py
@@ -1,1 +1,10 @@
-# empty file required by ESPHome
+import esphome.codegen as cg
+
+rc_switch_ns = cg.esphome_ns.namespace("rc_switch_component")
+
+cg.add_library("rc-switch", None)
+
+cg.add_global(cg.RawExpression("using namespace rc_switch_component;"))
+
+cg.add_source_file("rc_switch_component.cpp")
+cg.add_source_file("rc_switch_component.h")
