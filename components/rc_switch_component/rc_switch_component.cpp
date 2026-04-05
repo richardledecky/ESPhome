1ef584c92ecd03ca4f8e2fbdf11ebf6721510ca0
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:55:36 +0200

    Update rc_switch_component.cpp

--- components/rc_switch_component/rc_switch_component.cpp
+++ components/rc_switch_component/rc_switch_component.cpp
@@ -2,7 +2,25 @@
 
 namespace rc_switch_component {
 
-// zatiaľ prázdne, všetko je inline v .h
-// tu môžeš neskôr doplniť kód pre skutočný RF vysielač
+void RCSwitchTransmitter::setup() {
+  // inicializacia vysielaca
+}
+
+void RCSwitchTransmitter::loop() {
+  // loop, ak treba
+}
+
+void RCSwitchTransmitter::send_code(uint32_t code) {
+  // odoslanie RF kodu
+  ESP_LOGD("rc_switch", "Sending code: %u", code);
+}
+
+void SendRCSwitchAction::play(int repetitions) {
+  if (parent) {
+    for (int i = 0; i < repetitions; i++) {
+      parent->send_code(code);
+    }
+  }
+}
 
 }  // namespace rc_switch_component
