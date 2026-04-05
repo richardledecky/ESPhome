6bc340211e841686a9da6a4ed7f84fe7d18e331b
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:49:45 +0200

    Update rc_switch_component.h

--- components/rc_switch_component/rc_switch_component.h
+++ components/rc_switch_component/rc_switch_component.h
@@ -7,20 +7,21 @@ class RCSwitchTransmitter : public Component {
  public:
   void setup() override {}
   void loop() override {}
-
   void send_code(uint32_t code) {
-    // Tu pridáš kód na vysielanie 433MHz
     ESP_LOGD("rc_switch", "Sending code: %u", code);
+    // sem príde kód pre vysielanie RF
   }
 };
 
 class SendRCSwitchAction : public Action {
  public:
-  RCSwitchTransmitter *parent;
+  RCSwitchTransmitter* parent;
   uint32_t code;
 
-  void play(uint32_t play_duration) override {
-    if (parent) parent->send_code(code);
+  void play(uint32_t) override {
+    if (parent != nullptr) {
+      parent->send_code(code);
+    }
   }
 };
 
