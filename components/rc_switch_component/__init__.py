c1fb630b785976ad1002a4ab58ebc940f8a2ee84
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-06 14:14:59 +0200

    Update __init__.py

--- components/rc_switch_component/__init__.py
+++ components/rc_switch_component/__init__.py
@@ -1,8 +1,12 @@
-CODEOWNERS = ["@richardledecky"]
-DEPENDENCIES = []
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+CODEOWNERS = ["@richardledecky"]
+DEPENDENCIES = []
 
 rc_switch_ns = cg.esphome_ns.namespace("rc_switch_component")
 
@@ -27,10 +31,9 @@ RC_SWITCH_SEND_SCHEMA = cv.Schema(
     RC_SWITCH_SEND_SCHEMA,
 )
 async def rc_switch_send_to_code(config, action_id, template_arg, args):
+    _LOGGER.info("rc_switch_component action called!")
     var = cg.new_Pvariable(action_id, template_arg)
     cg.add(var.set_code(config[CONF_CODE]))
     cg.add(var.set_gpio(config[CONF_GPIO]))
     cg.add_library("sui77/rc-switch", None)
     return var
-_LOGGER = logging.getLogger(__name__)
-_LOGGER.info("rc_switch_component __init__.py loaded!")
