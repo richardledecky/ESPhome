5200eaaf05defff41ff1b74c0438bb4a5ab16274
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-06 14:18:32 +0200

    Update __init__.py

--- components/rc_switch_component/__init__.py
+++ components/rc_switch_component/__init__.py
@@ -10,12 +10,9 @@ DEPENDENCIES = []
 
 rc_switch_ns = cg.esphome_ns.namespace("rc_switch_component")
 
-# Action sa berie z esphome core namespace, nie z cg
 Action = cg.esphome_ns.class_("Action")
 
-SendRCSwitchAction = rc_switch_ns.class_(
-    "SendRCSwitchAction", Action
-)
+SendRCSwitchAction = rc_switch_ns.class_("SendRCSwitchAction", Action)
 
 CONF_CODE = "code"
 CONF_GPIO = "gpio"
@@ -25,17 +22,16 @@ CONFIG_SCHEMA = cv.Schema({})
 async def to_code(config):
     pass
 
-RC_SWITCH_SEND_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(SendRCSwitchAction),
-        cv.Required(CONF_CODE): cv.positive_int,
-        cv.Optional(CONF_GPIO, default=23): cv.int_,
-    }
-)
+RC_SWITCH_SEND_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SendRCSwitchAction),
+    cv.Required(CONF_CODE): cv.positive_int,
+    cv.Optional(CONF_GPIO, default=23): cv.int_,
+})
 
-@cg.register_action(
-    "rc_switch_component.send",
-    SendRCSwitchAction,
-    RC_SWITCH_SEND_SCHEMA,
-)
-async def rc_switch_send_to_code(config,
+@cg.register_action("rc_switch_component.send", SendRCSwitchAction, RC_SWITCH_SEND_SCHEMA)
+async def rc_switch_send_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    cg.add(var.set_code(config[CONF_CODE]))
+    cg.add(var.set_gpio(config[CONF_GPIO]))
+    cg.add_library("sui77/rc-switch", None)
+    return var
